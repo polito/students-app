d7e121d9eca078872d62b144ffc8ac483d25a8ad
Umberto Pepato <umbo.pepato@gmail.com>
2022-08-17 11:10:05 +0200

    feat: refactor SectionHeader to use design system components
    Refs #9

--- lib/ui/components/SectionHeader.tsx
+++ lib/ui/components/SectionHeader.tsx
@@ -1,17 +1,25 @@
 import { useTranslation } from 'react-i18next';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Link } from '@react-navigation/native';
 import { To } from '@react-navigation/native/lib/typescript/src/useLinkTo';
+import { useStylesheet } from '../hooks/useStylesheet';
 import { useTheme } from '../hooks/useTheme';
+import { Theme } from '../types/theme';
 import { Separator } from './Separator';
+import { Text } from './Text';
 
 interface Props {
   title: string;
-  linkTo: To;
+  linkTo?: To;
 }
 
+/**
+ * A section title with an optional link to a related
+ * screen
+ */
 export const SectionHeader = ({ title, linkTo }: Props) => {
   const { colors } = useTheme();
+  const styles = useStylesheet(createStyles);
   const { t } = useTranslation();
 
   return (
@@ -19,43 +27,35 @@ export const SectionHeader = ({ title, linkTo }: Props) => {
       <Separator />
       <View style={styles.innerContainer}>
         <Text
-          style={[
-            styles.title,
-            {
-              color: colors.headings,
-            },
-          ]}
+          variant="title"
+          style={{
+            color: colors.heading,
+          }}
+          accessible={true}
+          accessibilityRole="header"
         >
           {title}
         </Text>
-        <Link
-          to={linkTo}
-          style={{
-            color: colors.links,
-          }}
-        >
-          {t('See all')}
-        </Link>
+        {linkTo && (
+          <Link to={linkTo}>
+            <Text variant="link">{t('See all')}</Text>
+          </Link>
+        )}
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    paddingHorizontal: 18,
-  },
-  innerContainer: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'left',
-  },
-});
+const createStyles = ({ spacing }: Theme) =>
+  StyleSheet.create({
+    container: {
+      width: '100%',
+      paddingHorizontal: spacing[5],
+    },
+    innerContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+  });
