c0daad54998acfd99200053dfda79c67b2186b78
Umberto Pepato <umbo.pepato@gmail.com>
2022-08-17 11:13:00 +0200

    feat: implement Tabs components
    Refs #13

---
+++ lib/ui/components/Tabs.tsx
@@ -0,0 +1,59 @@
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  PropsWithChildren,
+  ReactElement,
+} from 'react';
+import { ScrollView, ScrollViewProps, View } from 'react-native';
+import { useTheme } from '../hooks/useTheme';
+import { Props as TabProps } from './Tab';
+
+interface Props {
+  selectedIndexes?: number[];
+}
+
+/**
+ * A horizontally scrolling tab bar.
+ * To control the selection state of child tabs set the `selectedIndexes`
+ * prop to an array of indexes. Leave it undefined to manually control the
+ * selection on the children.
+ */
+export const Tabs = ({
+  children,
+  selectedIndexes,
+  ...rest
+}: PropsWithChildren<ScrollViewProps & Props>) => {
+  const { spacing } = useTheme();
+
+  return (
+    <ScrollView
+      accessible={true}
+      accessibilityRole="tablist"
+      horizontal
+      contentContainerStyle={{
+        paddingHorizontal: spacing[4],
+        paddingVertical: spacing[2],
+      }}
+      {...rest}
+    >
+      {Children.map(children, (c: ReactElement<TabProps>, i) => {
+        if (isValidElement(c) && selectedIndexes != null) {
+          c = cloneElement(c, { selected: selectedIndexes.includes(i) });
+        }
+        return (
+          <>
+            {c}
+            {i < Children.count(children) - 1 && (
+              <View
+                style={{
+                  width: spacing[2],
+                }}
+              />
+            )}
+          </>
+        );
+      })}
+    </ScrollView>
+  );
+};
