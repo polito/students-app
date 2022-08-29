7a8bbcdc07f3736112b68b909a5a563ea327ec78
Umberto Pepato <umbo.pepato@gmail.com>
2022-08-29 20:25:16 +0200

    feat: implement CourseGuide screen

--- src/features/teaching/screens/CourseGuideScreen.tsx
+++ src/features/teaching/screens/CourseGuideScreen.tsx
@@ -1,5 +1,13 @@
-import { Text, View } from 'react-native';
+import { useMemo } from 'react';
+import { Platform, ScrollView } from 'react-native';
+import { Card } from '@lib/ui/components/Card';
+import { Section } from '@lib/ui/components/Section';
+import { SectionHeader } from '@lib/ui/components/SectionHeader';
+import { Text } from '@lib/ui/components/Text';
+import { useTheme } from '@lib/ui/hooks/useTheme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { createRefreshControl } from '../../../core/hooks/createRefreshControl';
+import { useBottomBarAwareStyles } from '../../../core/hooks/useBottomBarAwareStyles';
 import { TeachingStackParamList } from '../components/TeachingNavigator';
 import { useGetCourseGuide } from '../hooks/courseHooks';
 
@@ -7,17 +15,43 @@ type Props = NativeStackScreenProps<TeachingStackParamList, 'CourseGuide'>;
 
 export const CourseGuideScreen = ({ route }: Props) => {
   const { courseId } = route.params;
-  const { data: guideResponse } = useGetCourseGuide(courseId);
+  const { spacing } = useTheme();
+  const bottomBarAwareStyles = useBottomBarAwareStyles();
+  const guideQuery = useGetCourseGuide(courseId);
+  const guideSections = useMemo(
+    () =>
+      guideQuery.data?.data.map(section => ({
+        ...section,
+        content: section.content.replace(/(\\f|\\n)+/g, '\n\n').trim(),
+      })) ?? [],
+    [guideQuery],
+  );
 
   return (
-    <View>
-      {guideResponse &&
-        guideResponse.data.map((section, i) => (
-          <View key={i}>
-            <Text style={{ fontWeight: 'bold' }}>{section.title}</Text>
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      contentContainerStyle={{
+        paddingVertical: spacing[5],
+      }}
+      refreshControl={createRefreshControl(guideQuery)}
+      style={bottomBarAwareStyles}
+    >
+      {guideSections.map((section, i) => (
+        <Section key={i}>
+          <SectionHeader title={section.title} />
+          <Card
+            rounded={Platform.select({ android: false })}
+            style={{
+              marginVertical: spacing[2],
+              marginHorizontal: Platform.select({ ios: spacing[4] }),
+              paddingHorizontal: spacing[5],
+              paddingVertical: spacing[5],
+            }}
+          >
             <Text>{section.content}</Text>
-          </View>
-        ))}
-    </View>
+          </Card>
+        </Section>
+      ))}
+    </ScrollView>
   );
 };
