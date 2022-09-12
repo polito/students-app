80512916b7c204f2a5ee51f83771c33cc815a89e
luca.pezzolla <me@lucapezzolla.com>
2022-09-12 10:28:07 +0200

    feat: tentative videoplayer implementation

--- src/features/teaching/screens/CourseVirtualClassroomScreen.tsx
+++ src/features/teaching/screens/CourseVirtualClassroomScreen.tsx
@@ -1,4 +1,8 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import { Dimensions, Text, View } from 'react-native';
+import { ResizeMode } from 'expo-av';
+import VideoPlayer from 'expo-video-player';
+import { useTheme } from '@lib/ui/hooks/useTheme';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { TeachingStackParamList } from '../components/TeachingNavigator';
 import { useGetCourseVirtualClassrooms } from '../hooks/courseHooks';
@@ -15,8 +19,39 @@ export const CourseVirtualClassroomScreen = ({ route }: Props) => {
     useGetCourseVirtualClassrooms(courseId);
 
   const lecture = virtualClassroomsResponse?.data.find(l => l.id === lectureId);
+  const { width } = Dimensions.get('window');
+  const { colors } = useTheme();
+
   return (
     <View>
+      <VideoPlayer
+        defaultControlsVisible={true}
+        header={
+          <Text style={{ color: '#FFF' }}>Custom controls will go up here</Text>
+        }
+        slider={{
+          thumbTintColor: colors.secondary[600],
+          minimumTrackTintColor: colors.secondary[600],
+          maximumTrackTintColor: colors.background,
+        }}
+        style={{
+          height: (width / 16) * 9,
+          controlsBackgroundColor: colors.heading,
+        }}
+        videoProps={{
+          resizeMode: ResizeMode.CONTAIN,
+          posterSource: {
+            uri: lecture.coverUrl,
+          },
+          source: {
+            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+          },
+          status: {
+            rate: 2,
+          },
+        }}
+      />
+
       <Text>{lecture && JSON.stringify(lecture)}</Text>
     </View>
   );
