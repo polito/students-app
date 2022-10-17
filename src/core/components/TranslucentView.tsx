859bcfead0612d16709b1dd6e38bcd744f9bc7d2
luca.pezzolla <me@lucapezzolla.com>
2022-10-17 22:07:31 +0200

    fix: implement TranslucentView alternative for Android

--- src/core/components/TranslucentView.tsx
+++ src/core/components/TranslucentView.tsx
@@ -2,7 +2,16 @@ import { StyleSheet, ViewStyle, useColorScheme } from 'react-native';
 
 import { BlurView } from '@react-native-community/blur';
 
-export const TranslucentView = ({ style = null, blurAmount = 100 }) => {
+export interface TranslucentViewProps {
+  style?: ViewStyle;
+  blurAmount?: number;
+  opacity?: number;
+}
+
+export const TranslucentView = ({
+  style = null,
+  blurAmount = 5,
+}: TranslucentViewProps) => {
   const scheme = useColorScheme();
   return (
     <BlurView
