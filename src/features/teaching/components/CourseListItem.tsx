09b88a1386a417cf292a7a1ed3d659c3a812cbe1
Umberto Pepato <umbo.pepato@gmail.com>
2022-11-17 18:57:42 +0100

    feat: open quick course prefs menu from button on Android

--- src/features/teaching/components/CourseListItem.tsx
+++ src/features/teaching/components/CourseListItem.tsx
@@ -1,7 +1,11 @@
-import { useContext } from 'react';
+import { PropsWithChildren, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Platform } from 'react-native';
 
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { IconButton } from '@lib/ui/components/IconButton';
 import { ListItem } from '@lib/ui/components/ListItem';
+import { useTheme } from '@lib/ui/hooks/useTheme';
 import { CourseOverview } from '@polito/api-client';
 import { MenuView } from '@react-native-menu/menu';
 
@@ -12,14 +16,20 @@ interface Props {
   course: CourseOverview;
 }
 
-export const CourseListItem = ({ course }: Props) => {
+const Menu = ({
+  course,
+  shouldOpenOnLongPress,
+  children,
+}: PropsWithChildren<{
+  course: CourseOverview;
+  shouldOpenOnLongPress?: boolean;
+}>) => {
   const { t } = useTranslation();
   const preferences = useContext(PreferencesContext);
 
   return (
     <MenuView
-      key={course.shortcode}
-      shouldOpenOnLongPress={true}
+      shouldOpenOnLongPress={shouldOpenOnLongPress}
       title={t('Course preferences')}
       actions={[
         {
@@ -38,24 +48,61 @@ export const CourseListItem = ({ course }: Props) => {
         });
       }}
     >
-      <ListItem
-        linkTo={
-          course.id != null
-            ? {
-                screen: 'Course',
-                params: { id: course.id, courseName: course.name },
-              }
-            : undefined
-        }
-        title={course.name}
-        subtitle={`${course.cfu} ${t('common.credits').toLowerCase()}`}
-        leadingItem={
-          <CourseIcon
-            icon={preferences.courses[course.id]?.icon}
-            color={preferences.courses[course.id]?.color}
-          />
-        }
-      />
+      {children}
     </MenuView>
   );
+};
+
+export const CourseListItem = ({ course }: Props) => {
+  const { colors, spacing } = useTheme();
+  const { t } = useTranslation();
+  const preferences = useContext(PreferencesContext);
+
+  const listItem = (
+    <ListItem
+      linkTo={
+        course.id != null
+          ? {
+              screen: 'Course',
+              params: { id: course.id, courseName: course.name },
+            }
+          : undefined
+      }
+      title={course.name}
+      subtitle={`${course.cfu} ${t('common.credits').toLowerCase()}`}
+      leadingItem={
+        <CourseIcon
+          icon={preferences.courses[course.id]?.icon}
+          color={preferences.courses[course.id]?.color}
+        />
+      }
+      trailingItem={Platform.select({
+        android: (
+          <Menu course={course}>
+            <IconButton
+              style={{
+                padding: spacing[3],
+              }}
+              icon={faEllipsisVertical}
+              color={colors.secondaryText}
+            />
+          </Menu>
+        ),
+      })}
+      containerStyle={{
+        paddingRight: Platform.select({
+          android: 0,
+        }),
+      }}
+    />
+  );
+
+  if (Platform.OS === 'ios') {
+    return (
+      <Menu course={course} shouldOpenOnLongPress={true}>
+        {listItem}
+      </Menu>
+    );
+  }
+  return listItem;
 };
