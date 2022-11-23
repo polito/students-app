d590ba6554f190a20905c4cfbcf30866e06a18a2
luca.pezzolla <me@lucapezzolla.com>
2022-11-23 13:13:12 +0100

    fix(course/guide): do not represent empty sections

--- src/features/teaching/screens/CourseGuideScreen.tsx
+++ src/features/teaching/screens/CourseGuideScreen.tsx
@@ -6,6 +6,7 @@ import { Section } from '@lib/ui/components/Section';
 import { SectionHeader } from '@lib/ui/components/SectionHeader';
 import { Text } from '@lib/ui/components/Text';
 import { useTheme } from '@lib/ui/hooks/useTheme';
+import { CourseGuideSection } from '@polito/api-client/models/CourseGuideSection';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { useBottomBarAwareStyles } from '../../../core/hooks/useBottomBarAwareStyles';
@@ -21,14 +22,18 @@ export const CourseGuideScreen = ({ route }: Props) => {
   const bottomBarAwareStyles = useBottomBarAwareStyles();
   const guideQuery = useGetCourseGuide(courseId);
   const refreshControl = useRefreshControl(guideQuery);
-  const guideSections = useMemo(
-    () =>
-      guideQuery.data?.data.map(section => ({
-        ...section,
-        content: section.content.replace(/[\f\n]+/g, '\n').trim(),
-      })) ?? [],
-    [guideQuery],
-  );
+  const guideSections = useMemo(() => {
+    const sections: CourseGuideSection[] = [];
+
+    guideQuery.data?.data.forEach(section => {
+      const content = section.content.replace(/[\f\n]+/g, '\n').trim();
+
+      // Remove empty sections
+      if (content) sections.push({ ...section, content });
+    });
+
+    return sections;
+  }, [guideQuery]);
 
   return (
     <ScrollView
