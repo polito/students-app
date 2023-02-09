8728d51aaf37dee4ebe7e6f4a48f59c4dee82464
Davide Carizzoni <davide.carizzoni@runelab.it>
2023-02-09 16:39:24 +0100

    feat(tickets): detail

--- src/features/teaching/components/TicketListItem.tsx
+++ src/features/teaching/components/TicketListItem.tsx
@@ -1,12 +1,21 @@
+import { useTranslation } from 'react-i18next';
+import { Alert } from 'react-native';
+
 import { faComments } from '@fortawesome/free-regular-svg-icons';
-import { faChevronRight, faLink } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronRight,
+  faEllipsisVertical,
+  faLink,
+} from '@fortawesome/free-solid-svg-icons';
 import { Col } from '@lib/ui/components/Col';
 import { Icon } from '@lib/ui/components/Icon';
+import { IconButton } from '@lib/ui/components/IconButton';
 import { ListItem } from '@lib/ui/components/ListItem';
 import { Row } from '@lib/ui/components/Row';
 import { Text } from '@lib/ui/components/Text';
 import { useTheme } from '@lib/ui/hooks/useTheme';
 import { TicketOverview } from '@polito/api-client';
+import { MenuView } from '@react-native-menu/menu';
 
 import { IS_IOS } from '../../../utils/const';
 import { formatDateTime } from '../../../utils/dates';
@@ -17,7 +26,17 @@ interface Props {
 
 export const TicketListItem = ({ ticket }: Props) => {
   const { fontSizes, colors, spacing } = useTheme();
+  const { t } = useTranslation();
   console.debug({ ticket });
+
+  const actions = [
+    {
+      title: t('tickets.close'),
+      color: 'red',
+      image: 'trash.fill',
+      imageColor: 'red',
+    },
+  ];
 
   const UnReadCount = () => {
     return (
@@ -38,40 +57,72 @@ export const TicketListItem = ({ ticket }: Props) => {
     );
   };
 
+  const onPressCloseTicket = () => {
+    Alert.alert(t('tickets.close'), t('tickets.close'), [
+      {
+        text: 'Confirm',
+        onPress: () => console.debug('Cancel Pressed'),
+        style: 'cancel',
+      },
+    ]);
+  };
+
   return (
-    <ListItem
-      linkTo={{
-        screen: 'Ticket',
-        params: { id: ticket.id },
-      }}
-      title={ticket.subject}
-      subtitle={`${formatDateTime(ticket.updatedAt)} - ${ticket.message}`}
-      subtitleStyle={{
-        fontSize: fontSizes.xs,
-      }}
-      leadingItem={<Icon icon={faComments} size={20} />}
-      trailingItem={
-        <Row noFlex alignCenter>
-          <Icon
-            icon={faLink}
-            size={20}
-            color={colors.text['400']}
-            style={[
-              ticket.unreadCount === 0 && { marginHorizontal: spacing['2'] },
-            ]}
-          />
-          {ticket.unreadCount > 0 && <UnReadCount />}
-          {IS_IOS && (
+    <MenuView
+      title={t('tickets.menuAction')}
+      actions={actions}
+      onPressAction={onPressCloseTicket}
+      shouldOpenOnLongPress={!IS_IOS}
+    >
+      <ListItem
+        linkTo={{
+          screen: 'Ticket',
+          params: { id: ticket.id },
+        }}
+        title={ticket.subject}
+        subtitle={`${formatDateTime(ticket.updatedAt)} - ${ticket.message}`}
+        subtitleStyle={{
+          fontSize: fontSizes.xs,
+        }}
+        leadingItem={<Icon icon={faComments} size={20} />}
+        trailingItem={
+          <Row noFlex alignCenter>
             <Icon
-              icon={faChevronRight}
-              color={colors.secondaryText}
-              style={{
-                marginRight: -spacing[1],
-              }}
+              icon={faLink}
+              size={20}
+              color={colors.text['400']}
+              style={[
+                ticket.unreadCount === 0 && { marginHorizontal: spacing['2'] },
+              ]}
             />
-          )}
-        </Row>
-      }
-    />
+            {ticket.unreadCount > 0 && <UnReadCount />}
+            {IS_IOS ? (
+              <Icon
+                icon={faChevronRight}
+                color={colors.secondaryText}
+                style={{
+                  marginRight: -spacing[1],
+                }}
+              />
+            ) : (
+              <MenuView
+                title={t('tickets.menuAction')}
+                actions={actions}
+                onPressAction={onPressCloseTicket}
+              >
+                <IconButton
+                  style={{
+                    marginRight: -spacing[1],
+                  }}
+                  icon={faEllipsisVertical}
+                  color={colors.secondaryText}
+                  size={fontSizes.xl}
+                />
+              </MenuView>
+            )}
+          </Row>
+        }
+      />
+    </MenuView>
   );
 };
