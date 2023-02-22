603a7504ab6f2f63a8b649860df2fdcecbab329d
Luca Pezzolla <me@lucapezzolla.com>
2023-02-22 03:50:02 +0100

    feat: add Appsignal to track app errors
    Ref #74

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import Appsignal from '@appsignal/javascript';
+import { ErrorBoundary } from '@appsignal/react';
+import { APPSIGNAL_API_KEY } from '@env';
+
 import { AppContent } from './core/components/AppContent';
 import { ApiProvider } from './core/providers/ApiProvider';
 import { DownloadsProvider } from './core/providers/DownloadsProvider';
@@ -5,18 +9,25 @@ import { PreferencesProvider } from './core/providers/PreferencesProvider';
 import { SplashProvider } from './core/providers/SplashProvider';
 import { UiProvider } from './core/providers/UiProvider';
 
+
+const appsignal = new Appsignal({
+  key: APPSIGNAL_API_KEY,
+});
+
 export const App = () => {
   return (
-    <SplashProvider>
-      <PreferencesProvider>
-        <UiProvider>
-          <ApiProvider>
-            <DownloadsProvider>
-              <AppContent />
-            </DownloadsProvider>
-          </ApiProvider>
-        </UiProvider>
-      </PreferencesProvider>
-    </SplashProvider>
+    <ErrorBoundary instance={appsignal}>
+      <SplashProvider>
+        <PreferencesProvider>
+          <UiProvider>
+            <ApiProvider>
+              <DownloadsProvider>
+                <AppContent />
+              </DownloadsProvider>
+            </ApiProvider>
+          </UiProvider>
+        </PreferencesProvider>
+      </SplashProvider>
+    </ErrorBoundary>
   );
 };
