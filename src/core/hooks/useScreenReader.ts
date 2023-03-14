fc419dee8bbd7bc4033bdeca3851269384d23149
Davide Carizzoni <davide.carizzoni@runelab.it>
2023-03-14 14:33:30 +0100

    feat(accessibility): announce on section click if enabled

---
+++ src/core/hooks/useScreenReader.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { AccessibilityInfo } from 'react-native';
+
+export const useScreenReader = () => {
+  const [enabled, setEnabled] = useState<boolean>(false);
+  useEffect(() => {
+    AccessibilityInfo.isScreenReaderEnabled().then(res => setEnabled(res));
+  }, []);
+
+  const announce = (message: string) => {
+    AccessibilityInfo.announceForAccessibility(message);
+  };
+
+  return {
+    isEnabled: enabled,
+    announce,
+  };
+};
