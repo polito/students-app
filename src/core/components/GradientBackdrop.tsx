e2ab491b3bed6cb8bbe8e5c0ea2123a6adfd03a7
Umberto Pepato <umbo.pepato@gmail.com>
2023-03-29 18:39:08 +0200

    fix(ui): video player backdrop not filling entire width despite preserveAspectRatio=none

--- src/core/components/GradientBackdrop.tsx
+++ src/core/components/GradientBackdrop.tsx
@@ -4,7 +4,7 @@ import { Defs, LinearGradient, Rect, Stop, Svg } from 'react-native-svg';
 export const GradientBackdrop = () => {
   return (
     <Svg
-      width="100%"
+      width="101%"
       height="100%"
       viewBox="0 0 100 100"
       preserveAspectRatio="none"
@@ -12,13 +12,13 @@ export const GradientBackdrop = () => {
     >
       <Defs>
         <LinearGradient id="grad" x1="0" y1="0" x2="0" y2="100%">
-          <Stop offset="0" stopColor="black" stopOpacity="0.9" />
+          <Stop offset="0" stopColor="black" stopOpacity="0.8" />
           <Stop offset="0.2" stopColor="black" stopOpacity="0.5" />
-          <Stop offset="0.3" stopColor="black" stopOpacity="0.3" />
+          <Stop offset="0.3" stopColor="black" stopOpacity="0.29" />
           <Stop offset="0.5" stopColor="black" stopOpacity="0.2" />
-          <Stop offset="0.7" stopColor="black" stopOpacity="0.3" />
+          <Stop offset="0.7" stopColor="black" stopOpacity="0.29" />
           <Stop offset="0.8" stopColor="black" stopOpacity="0.5" />
-          <Stop offset="1" stopColor="black" stopOpacity="0.9" />
+          <Stop offset="1" stopColor="black" stopOpacity="0.8" />
         </LinearGradient>
       </Defs>
       <Rect x="0" y="0" width="100%" height="100%" fill="url(#grad)" />
