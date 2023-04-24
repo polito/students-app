f80946137035f1fe2d34995bf058683e848b2c35
Luca Pezzolla <me@lucapezzolla.com>
2023-04-24 12:38:06 +0200

    chore: restore ability to use mock api via .env

--- src/config/api.tsx
+++ src/config/api.tsx
@@ -1,3 +1,4 @@
+import { API_BASE_PATH } from '@env';
 import {
   AuthApi,
   BASE_PATH,
@@ -14,18 +15,13 @@ import {
 } from '@polito/api-client';
 
 import { ApiContextClientsProps } from '../core/contexts/ApiContext';
-import { deviceLanguage } from '../utils/device';
 
 export const createApiConfiguration = (token?: string) => {
-  const basePath = BASE_PATH;
+  const basePath = API_BASE_PATH ?? BASE_PATH;
   console.debug(`Expecting a running API at ${basePath}`);
 
   const configurationParameters: ConfigurationParameters = {
     basePath,
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      'Accept-Language': deviceLanguage, // TODO refactor
-    },
   };
 
   if (token) {
