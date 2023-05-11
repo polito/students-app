29c1a1617aacadba6e6153e2b5f276036abb73c6
Luca Pezzolla <me@lucapezzolla.com>
2023-05-11 11:56:14 +0200

    ci: create release and upload source maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import DeviceInfo from 'react-native-device-info';
+
 import * as Sentry from '@sentry/react-native';
 
 import { AppContent } from './core/components/AppContent';
@@ -8,7 +10,10 @@ import { SplashProvider } from './core/providers/SplashProvider';
 import { UiProvider } from './core/providers/UiProvider';
 
 Sentry.init({
-  dsn: 'https://6b860b219cbe49daa89a10a5d20f4d79@sentry.k8s.polito.it/2',
+  // dsn: 'https://6b860b219cbe49daa89a10a5d20f4d79@sentry.k8s.polito.it/2',
+  enabled: process.env.NODE_ENV === 'production',
+  dist: DeviceInfo.getBuildNumber(),
+  release: DeviceInfo.getVersion(),
 });
 
 export const App = () => {
