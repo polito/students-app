a6f6726a43e0c717152bc12033c1c434dfc3caa6
davidecarizzoni <davide.carizzoni@runelab.it>
2023-05-30 23:27:26 +0200

    feat(news): fix news, improve accessibility(#254)

--- src/features/services/screens/NewsItemScreen.tsx
+++ src/features/services/screens/NewsItemScreen.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { Linking, ScrollView, StyleSheet } from 'react-native';
+import { Linking, Pressable, ScrollView, StyleSheet } from 'react-native';
 import FastImage from 'react-native-fast-image';
 import { Document } from 'react-native-render-html';
 
@@ -12,6 +12,7 @@ import { ActivityIndicator } from '@lib/ui/components/ActivityIndicator';
 import { Card } from '@lib/ui/components/Card';
 import { Col } from '@lib/ui/components/Col';
 import { Icon } from '@lib/ui/components/Icon';
+import { ImageLoader } from '@lib/ui/components/ImageLoader';
 import { RefreshControl } from '@lib/ui/components/RefreshControl';
 import { Row } from '@lib/ui/components/Row';
 import { ScreenTitle } from '@lib/ui/components/ScreenTitle';
@@ -36,7 +37,7 @@ export const NewsItemScreen = ({ route }: Props) => {
   const { palettes, spacing } = useTheme();
   const { t } = useTranslation();
   const useNewsItemQuery = useGetNewsItem(id);
-  const { data: newsItem } = useNewsItemQuery;
+  const { data: newsItem, isLoading } = useNewsItemQuery;
   const styles = useStylesheet(createStyles);
   const { title, eventStartTime, createdAt, htmlContent } = newsItem || {};
 
@@ -46,8 +47,6 @@ export const NewsItemScreen = ({ route }: Props) => {
     e => e.type === 'image' && e.description === 'Logo1',
   );
 
-  console.debug('links', links, logo);
-
   return (
     <ScrollView
       contentInsetAdjustmentBehavior="automatic"
@@ -55,7 +54,7 @@ export const NewsItemScreen = ({ route }: Props) => {
     >
       <Section>
         <ScreenTitle title={title ?? ''} style={styles.heading} />
-        {useNewsItemQuery?.isLoading && (
+        {isLoading && (
           <ActivityIndicator
             style={{
               marginVertical: spacing[6],
@@ -63,65 +62,90 @@ export const NewsItemScreen = ({ route }: Props) => {
           />
         )}
       </Section>
-      {createdAt && (
-        <Card accessible padded>
-          <Text variant="heading" weight="semibold" numberOfLines={1}>
-            {t('newsScreen.createdAt')}
-            {formatDate(createdAt)}
-          </Text>
-        </Card>
-      )}
-      {!!logo && (
-        <Card accessible>
-          <FastImage
-            source={{ uri: logo.url }}
-            resizeMode={FastImage.resizeMode.cover}
-            style={{ height: 200, margin: spacing[3] }}
-          />
-        </Card>
-      )}
-      {htmlContent && (
-        <Card accessible>
-          <HtmlView source={{ dom }} />
-        </Card>
-      )}
-      <Card padded>
-        <Text variant="subHeading" weight="semibold">
-          {t('newsScreen.information')}
-        </Text>
-        <Col>
-          {eventStartTime && (
-            <Row style={styles.infoRow}>
-              <Icon
-                icon={faCalendarAlt}
-                style={styles.iconCalendar}
-                color={palettes.secondary[600]}
-              />
-              <Text weight="normal">
-                {formatDateFromString(eventStartTime)}
+      {!isLoading && (
+        <>
+          {createdAt && (
+            <Card accessible padded>
+              <Text variant="heading" weight="semibold" numberOfLines={1}>
+                {t('newsScreen.createdAt')}
+                {formatDate(createdAt)}
               </Text>
-            </Row>
+            </Card>
           )}
-          {links?.map((link, index) => (
-            <Row style={styles.infoRow} key={index}>
-              <Icon
-                icon={link.type === 'link' ? faInfoCircle : faFileAlt}
-                style={styles.iconCalendar}
-                color={palettes.secondary[600]}
+          {!!logo && (
+            <Card
+              accessible
+              accessibilityLabel={t('newsScreen.logo')}
+              accessibilityRole="image"
+            >
+              <ImageLoader
+                source={{ uri: logo.url }}
+                imageStyle={{ height: 200 }}
+                containerStyle={{ height: 200, margin: spacing[3] }}
+                resizeMode={FastImage.resizeMode.cover}
               />
-              <Text
-                numberOfLines={1}
-                weight="normal"
-                variant="link"
-                style={styles.link}
-                onPress={() => Linking.openURL(link.url)}
-              >
-                {link.description}
-              </Text>
-            </Row>
-          ))}
-        </Col>
-      </Card>
+            </Card>
+          )}
+          {htmlContent && (
+            <Card accessible>
+              <HtmlView source={{ dom }} />
+            </Card>
+          )}
+          <Card padded>
+            <Text
+              variant="subHeading"
+              weight="semibold"
+              style={{ marginBottom: spacing[1] }}
+            >
+              {t('newsScreen.information')}
+            </Text>
+            <Col>
+              {eventStartTime && (
+                <Row style={styles.infoRow} accessible accessibilityRole="text">
+                  <Icon
+                    icon={faCalendarAlt}
+                    style={styles.iconCalendar}
+                    color={palettes.secondary[600]}
+                  />
+                  <Text
+                    weight="normal"
+                    accessibilityLabel={[
+                      t('newsScreen.eventStartTime'),
+                      formatDateFromString(eventStartTime),
+                    ].join(' ')}
+                  >
+                    {formatDateFromString(eventStartTime)}
+                  </Text>
+                </Row>
+              )}
+              {links?.map((link, index) => (
+                <Pressable
+                  onPress={() => Linking.openURL(link.url)}
+                  key={index}
+                  accessible
+                  accessibilityRole="link"
+                >
+                  <Row style={styles.infoRow}>
+                    <Icon
+                      icon={link.type === 'link' ? faInfoCircle : faFileAlt}
+                      style={styles.iconCalendar}
+                      color={palettes.secondary[600]}
+                    />
+                    <Text
+                      numberOfLines={1}
+                      weight="normal"
+                      variant="link"
+                      style={styles.link}
+                    >
+                      {link.description}
+                    </Text>
+                  </Row>
+                </Pressable>
+              ))}
+            </Col>
+          </Card>
+        </>
+      )}
     </ScrollView>
   );
 };
@@ -133,7 +157,8 @@ const createStyles = ({ spacing }: Theme) =>
       paddingTop: spacing[3],
     },
     infoRow: {
-      marginTop: spacing[2],
+      marginVertical: spacing[1],
+      paddingVertical: spacing[1],
     },
     iconCalendar: {
       marginRight: spacing[2],
