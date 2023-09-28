9f8d764dc19a8274ac43d1be3fff2c2beae30be6
peppeplaza <giuseppe.piazza@polito.it>
2023-09-28 16:07:34 +0200

    refactor(ui): add CarouselDots component

---
+++ lib/ui/components/CarouselDots.tsx
@@ -0,0 +1,69 @@
+import { StyleSheet } from 'react-native';
+import AnimatedDotsCarousel from 'react-native-animated-dots-carousel';
+
+import { useStylesheet } from '../hooks/useStylesheet';
+import { Theme } from '../types/Theme';
+
+type Props = {
+  carouselLength: number;
+  carouselIndex: number;
+  expandedDotsCounts?: number;
+  hasDecreasingDots?: boolean;
+};
+
+export const CarouselDots = ({
+  carouselLength,
+  carouselIndex,
+  expandedDotsCounts = carouselLength,
+}: Props) => {
+  const styles = useStylesheet(createStyles);
+
+  return (
+    <AnimatedDotsCarousel
+      length={carouselLength ?? 0}
+      currentIndex={carouselIndex}
+      maxIndicators={expandedDotsCounts}
+      activeIndicatorConfig={{
+        ...styles.indicator,
+        ...styles.activeIndicator,
+      }}
+      inactiveIndicatorConfig={{
+        ...styles.indicator,
+        ...styles.inactiveIndicator,
+      }}
+      decreasingDots={[
+        {
+          config: {
+            ...styles.indicator,
+            ...styles.inactiveIndicator,
+            size: 8,
+          },
+          quantity: 1,
+        },
+        {
+          config: {
+            ...styles.indicator,
+            ...styles.inactiveIndicator,
+            size: 6,
+          },
+          quantity: 1,
+        },
+      ]}
+    />
+  );
+};
+
+const createStyles = ({ palettes, dark }: Theme) =>
+  StyleSheet.create({
+    indicator: {
+      margin: 3,
+      opacity: 1,
+      size: 10,
+    },
+    activeIndicator: {
+      color: palettes.navy[500],
+    },
+    inactiveIndicator: {
+      color: dark ? palettes.gray[400] : palettes.gray[300],
+    },
+  });
