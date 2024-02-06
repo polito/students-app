af172b2ed100bee793a829e02b320c4db3281876
Luca Pezzolla <me@lucapezzolla.com>
2024-02-07 00:54:22 +0100

    fix(ui): show selected state in menu view for Android devices

---
+++ lib/ui/components/StatefulMenuView.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useMemo } from 'react';
+
+import { MenuAction, MenuView } from '@react-native-menu/menu';
+import { MenuComponentProps } from '@react-native-menu/menu/src/types';
+
+import { IS_ANDROID } from '../../../src/core/constants';
+
+export const StatefulMenuView = ({
+  actions,
+  children,
+  ...props
+}: MenuComponentProps) => {
+  const mapAction = useCallback((action: MenuAction) => {
+    if (action.state === 'on') {
+      return {
+        ...action,
+        title: `✓ ${action.title}`,
+      };
+    }
+    return action;
+  }, []);
+
+  const effectiveActions = useMemo(() => {
+    if (!IS_ANDROID) return actions;
+
+    return actions.map(action => {
+      if (action.subactions) {
+        return {
+          ...action,
+          subactions: action.subactions.map(mapAction),
+        };
+      }
+      return mapAction(action);
+    });
+  }, [actions, mapAction]);
+  return (
+    <MenuView actions={effectiveActions} {...props}>
+      {children}
+    </MenuView>
+  );
+};
