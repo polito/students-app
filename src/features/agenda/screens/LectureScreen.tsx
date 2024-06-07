3f807b0dde2f0e4fde13d5eecf386ff6743296bf
Emanuele Coricciati <emanuelecoricciati@libero.it>
2024-06-07 11:07:37 +0200

    fix(ui): fix full screen on multiple lectures android, stop video when tab changes

--- src/features/agenda/screens/LectureScreen.tsx
+++ src/features/agenda/screens/LectureScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   Alert,
@@ -19,6 +19,7 @@ import { PersonListItem } from '@lib/ui/components/PersonListItem';
 import { Swiper } from '@lib/ui/components/Swiper';
 import { useTheme } from '@lib/ui/hooks/useTheme';
 import { VirtualClassroom } from '@polito/api-client/models/VirtualClassroom';
+import { useNavigation } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { DateTime, WeekdayNumbers } from 'luxon';
@@ -57,11 +58,46 @@ export const LectureScreen = ({ route, navigation }: Props) => {
   }, [lecture?.uniqueShortcode, coursesPrefs]);
 
   const { width } = useWindowDimensions();
+  const { addListener } = useNavigation();
+  const [currentIndex, setCurrentVideoIndex] = useState<number>(0);
+  const [isPiP, setIsPiP] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    return addListener('blur', () => {
+      if (isPiP) return;
+      setPlayingVC(oldVC => [
+        ...oldVC.map((v, i) => {
+          if (i === currentIndex) {
+            v.isPaused = false;
+          }
+          return v;
+        }),
+      ]);
+      setTimeout(() => {
+        setPlayingVC(oldVC => [
+          ...oldVC.map((v, i) => {
+            if (i === currentIndex) {
+              v.isPaused = true;
+            }
+            return v;
+          }),
+        ]);
+      });
+    });
+  }, [currentIndex, isPiP, addListener]);
+
+  const onPictureInPictureStatusChanged = useCallback(
+    ({ isActive }: { isActive: boolean }) => {
+      setIsPiP(isActive);
+    },
+    [],
+  );
 
   const [currentVideoTitle, setCurrentVideoTitle] = useState<string>();
 
   const handleSetCurrentPageIndex = (newIndex: number, oldIndex: number) => {
-    // setCurrentVideoIndex(newIndex);
+    setCurrentVideoIndex(newIndex);
     setPlayingVC(oldVC => [
       ...oldVC.map((v, i) => {
         if (i === newIndex) {
@@ -69,19 +105,25 @@ export const LectureScreen = ({ route, navigation }: Props) => {
           v.isPaused = false;
         }
         if (i === oldIndex) v.isPaused = true;
-
         return v;
       }),
     ]);
   };
 
-  const renderItem = ({ item }: ListRenderItemInfo<PlayingVC>) => {
+  const toggleFullScreen = useCallback(() => {
+    setIsFullScreen(prev => !prev);
+  }, []);
+
+  const renderItem = ({ item, index }: ListRenderItemInfo<PlayingVC>) => {
     return (
       <View style={{ width }}>
         <VideoPlayer
           source={{ uri: item.videoUrl }}
           poster={item.coverUrl ?? undefined}
           paused={item.isPaused}
+          pictureInPicture={index === currentIndex}
+          onPictureInPictureStatusChanged={onPictureInPictureStatusChanged}
+          toggleFullScreen={toggleFullScreen}
         />
       </View>
     );
@@ -170,6 +212,7 @@ export const LectureScreen = ({ route, navigation }: Props) => {
             )}
           {playingVC && playingVC.length > 1 && (
             <Swiper
+              isFullScreen={isFullScreen}
               items={playingVC}
               renderItem={renderItem}
               keyExtractor={item => item.id.toString()}
