8920074da7e7f3e1a2301a0166e51476b19c6b80
Emanuele Coricciati <emanuelecoricciati@libero.it>
2024-10-14 12:05:06 +0200

    fix(tickets): fix issues in ticket format, update regex for links

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -2,6 +2,7 @@ import { Document } from 'react-native-render-html';
 
 import { innerText } from 'domutils';
 import { parseDocument } from 'htmlparser2';
+import i18next from 'i18next';
 
 export const sanitizeHtml = (html: string) =>
   html.replace(/\r/g, '').replace(/\\r/g, '').replace(/\\"/g, '"').trim();
@@ -11,9 +12,16 @@ export const getHtmlTextContent = (text: string) => {
   return innerText(dom.children as any[]);
 };
 
+export const replaceImgWithAnchorTags = (html: string) => {
+  const imgRegex = /<img[^>]*src="([^">]*)"[^>]*>/gi;
+  return html.replace(imgRegex, (_, src) => {
+    return `<a href="${src}">${i18next.t('common.image')}</a>`;
+  });
+};
+
 export const linkUrls = (html: string) => {
   const regex =
-    /(?!<a[^>]*>[^<])(?:https?:\/\/|www\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])(?![^<]*<\/a>)/gi;
+    /(?!<a[^>]*>[^<])(?:https?:\/\/(?:www\.)?|www\.)?(?:[A-Z0-9-]+(?:[-.][A-Z0-9]+)*\.[A-Z]{2,5})(?::[0-9]{1,5})?(?:\/[A-Z0-9\-._~:/?#[\]@!$&'()*+,;=]*)?(?:\?[A-Z0-9\-._~:/?#[\]@!$&'()*+,;=%]*)?(?:#[A-Z0-9\-._~:/?#[\]@!$&'()*+,;=%]*)?(?![^<]*<\/a>)(?<!\.)/gi;
   return html.replace(regex, match => {
     if (!match.startsWith('http')) match = `https://${match}`;
     return `<a href="${match}">${match}</a>`;
