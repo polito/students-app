9b68f5d64bacbeb4b6a808b421c893cf50dc8270
alessandropremoli <alessandro.premoli@gmail.com>
2024-11-27 17:02:25 +0100

    fix(guide-screen): accessibility

--- src/features/guides/components/GuideFieldListItem.tsx
+++ src/features/guides/components/GuideFieldListItem.tsx
@@ -28,7 +28,7 @@ export const GuideFieldListItem = ({ field }: Props) => {
   return (
     <ListItem
       accessible={true}
-      accessibilityLabel={[field.label, ' - ', field.value].join(' ')}
+      accessibilityLabel={[field.label, ' - ', field.value].join(', ')}
       title={
         <View style={styles.row}>
           <Text style={[styles.text, styles.label]}>{field.label}</Text>
