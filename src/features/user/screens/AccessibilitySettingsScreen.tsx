395a330d7654514cb3a5e1e565dd7e2dfe21c217
miky41195 <thatsmickeyy@gmail.com>
2025-02-04 10:54:22 +0100

    feat(ui): added font size accessibility setting + fix some warnings in console

--- src/features/user/screens/AccessibilitySettingsScreen.tsx
+++ src/features/user/screens/AccessibilitySettingsScreen.tsx
@@ -20,7 +20,7 @@ import { usePreferencesContext } from '../../../core/contexts/PreferencesContext
 interface AccessibilityItemProps {
   t: TFunction;
   value?: string;
-  onUpdate: (event: string) => void;
+  onUpdate: (event: any) => void;
 }
 
 export const AccessibilitySettingsScreen = () => {
@@ -62,6 +62,23 @@ export const AccessibilitySettingsScreen = () => {
                     return updatePreference('accessibility', {
                       ...accessibility,
                       fontPlacement: e,
+                    });
+                }}
+              />
+              <CustomFontSizeListItem
+                t={t}
+                value={accessibility?.fontSize?.toString()}
+                onUpdate={e => {
+                  if (
+                    Number(e) === 100 ||
+                    Number(e) === 125 ||
+                    Number(e) === 150 ||
+                    Number(e) === 175 ||
+                    Number(e) === 200
+                  )
+                    return updatePreference('accessibility', {
+                      ...accessibility,
+                      fontSize: e,
                     });
                 }}
               />
@@ -262,6 +279,74 @@ const CustomFontPlacementListItem = ({
         title={t(`accessibilitySettingsScreen.customFontPlacement`)}
         subtitle={effectiveLabel}
         // subtitleProps={{ capitalize: true }}
+        /* TODO accessibilityLabel={`${t('common.language')}: ${t(
+          `common.${language}`,
+        )}. ${t('settingsScreen.openLanguageMenu')}`}*/
+      />
+    </MenuView>
+  );
+};
+const CustomFontSizeListItem = ({
+  t,
+  value,
+  onUpdate,
+}: AccessibilityItemProps) => {
+  const choices = useMemo(() => {
+    return [
+      {
+        label: '100%',
+        value: 100,
+      },
+      {
+        label: '125%',
+        value: 125,
+      },
+      {
+        label: '150%',
+        value: 150,
+      },
+      {
+        label: '175%',
+        value: 175,
+      },
+      {
+        label: '200%',
+        value: 200,
+      },
+    ];
+  }, []);
+
+  const effectiveValue = useMemo(() => {
+    return value || '100';
+  }, [value]);
+
+  const effectiveLabel = useMemo(() => {
+    return effectiveValue + '%';
+  }, [effectiveValue]);
+
+  const actions: MenuAction[] = useMemo(() => {
+    return choices.map(cc => {
+      const actionValue = cc.value === 100 ? '100' : cc.value.toString();
+      return {
+        id: actionValue,
+        title: cc.label,
+        state: actionValue === effectiveValue ? 'on' : undefined,
+      };
+    });
+  }, [effectiveValue, choices]);
+  return (
+    <MenuView
+      title={t(`accessibilitySettingsScreen.customFontSizeTitle`)}
+      actions={actions}
+      onPressAction={({ nativeEvent: { event } }) => {
+        onUpdate(event);
+      }}
+    >
+      <ListItem
+        isAction
+        title={t(`accessibilitySettingsScreen.customFontSizeTitle`)}
+        subtitle={effectiveLabel}
+        // // subtitleProps={{ capitalize: true }}
         /* TODO accessibilityLabel={`${t('common.language')}: ${t(
           `common.${language}`,
         )}. ${t('settingsScreen.openLanguageMenu')}`}*/
