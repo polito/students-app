4492cb310199877832865c5d826ecbe60367cf95
miky41195 <thatsmickeyy@gmail.com>
2025-02-10 16:22:23 +0100

    fix(courses): removed pdfViewerScreen and added feedback path downloaded file

--- src/features/courses/navigation/CourseSharedScreens.tsx
+++ src/features/courses/navigation/CourseSharedScreens.tsx
@@ -11,7 +11,6 @@ import {
   NativeStackNavigationOptions,
 } from '@react-navigation/native-stack';
 
-import { PdfViewerScreen } from '../../../../src/features/user/screens/PdfViewerScreen';
 import { CourseAssignmentPdfCreationScreen } from '../screens/CourseAssignmentPdfCreationScreen';
 import { CourseAssignmentUploadConfirmationScreen } from '../screens/CourseAssignmentUploadConfirmationScreen';
 import { CourseAssignmentUploadScreen } from '../screens/CourseAssignmentUploadScreen';
