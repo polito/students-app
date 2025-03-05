ef25085ec0ca53958d0b315abb2f6af2561f0276
miky41195 <thatsmickeyy@gmail.com>
2025-03-05 16:38:48 +0100

    feat: add structure for deep linking (WIP)

--- src/features/places/components/PlacesNavigator.tsx
+++ src/features/places/components/PlacesNavigator.tsx
@@ -131,7 +131,6 @@ export const PlacesNavigator = () => {
       setFloorId(id);
     }
   };
-
   useEffect(() => {
     request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
     request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
