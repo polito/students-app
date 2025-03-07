d440e8dbdd92eff759d55d9c504ffe852604394a
miky41195 <thatsmickeyy@gmail.com>
2025-03-07 11:33:08 +0100

    feat(access): add SSO Structure (WIP)

--- src/core/providers/UiProvider.tsx
+++ src/core/providers/UiProvider.tsx
@@ -1,17 +1,17 @@
-import { PropsWithChildren, useEffect, useMemo, useState } from 'react';
+import { PropsWithChildren, useEffect, useMemo } from 'react';
 import { initReactI18next } from 'react-i18next';
 import { Linking, Platform, StatusBar, useColorScheme } from 'react-native';
 import overrideColorScheme from 'react-native-override-color-scheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { ThemeContext } from '@lib/ui/contexts/ThemeContext';
-import { LinkingOptions } from '@react-navigation/native';
 
 import i18n from 'i18next';
 import { Settings } from 'luxon';
 
 import en from '../../../assets/translations/en.json';
 import it from '../../../assets/translations/it.json';
+import { setDeepLink } from '../../../src/utils/linking';
 import { fromUiTheme } from '../../utils/navigation-theme';
 import { NavigationContainer } from '../components/NavigationContainer';
 import { usePreferencesContext } from '../contexts/PreferencesContext';
@@ -37,12 +37,6 @@ export const UiProvider = ({ children }: PropsWithChildren) => {
   const safeAreaInsets = useSafeAreaInsets();
   const theme = useColorScheme();
   const { isAppLoaded } = useSplashContext();
-  const [, setParams] = useState({
-    bl_id: '',
-    fl_id: '',
-    rm_id: '',
-    placeId: '',
-  });
   useEffect(() => {
     if (colorScheme === 'dark' || colorScheme === 'light') {
       overrideColorScheme.setScheme(colorScheme);
@@ -61,52 +55,7 @@ export const UiProvider = ({ children }: PropsWithChildren) => {
   }, [colorScheme, theme, safeAreaInsets]);
 
   const navigationTheme = useMemo(() => fromUiTheme(uiTheme), [uiTheme]);
-  const linking: LinkingOptions<any> = {
-    prefixes: ['polito://students'],
-    config: {
-      screens: {
-        PlacesTab: {
-          screens: {
-            Place: {
-              path: 'PlacesTab/Place/:placeId',
 
-              // https://www.polito.it/mappe?bl_id=TO_CEN03&fl_id=XS01&rm_id=L003
-              parse: {
-                bl_id: (bl_id: string) => {
-                  setParams(prevState => {
-                    prevState.bl_id = bl_id;
-                    return prevState;
-                  });
-                  return `${bl_id}`;
-                },
-                fl_id: (fl_id: string) => {
-                  setParams(prevState => {
-                    prevState.fl_id = fl_id;
-                    return prevState;
-                  });
-                  return `${fl_id}`;
-                },
-                rm_id: (rm_id: string) => {
-                  setParams(prevState => {
-                    prevState.rm_id = rm_id;
-                    return prevState;
-                  });
-                  return `${rm_id}`;
-                },
-                placeId: (placeId: string) => {
-                  setParams(prevState => {
-                    prevState.placeId = placeId;
-                    return prevState;
-                  });
-                  return `${placeId}`;
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-  };
   useEffect(() => {
     i18n.changeLanguage(language);
     Settings.defaultLocale = language;
@@ -117,16 +66,8 @@ export const UiProvider = ({ children }: PropsWithChildren) => {
     const GoToUrlOnMap = () => {
       Linking.getInitialURL().then(url => {
         if (url) {
-          const route = url
-            .replace('polito://students/PlacesTab/', '')
-            .split('/');
-          if (route.length === 2) {
-            const placeId = `${route[1]}`;
-            const newUrl = `polito://students/PlacesTab/Place/${placeId}`;
-            if (isAppLoaded) {
-              Linking.openURL(newUrl);
-              // TO_CEN05&fl_id=XPTE&rm_id=B056
-            }
+          if (isAppLoaded) {
+            Linking.openURL(url);
           }
         }
       });
@@ -144,7 +85,7 @@ export const UiProvider = ({ children }: PropsWithChildren) => {
           ios: theme === 'dark' ? 'light-content' : 'dark-content',
         })}
       />
-      <NavigationContainer linking={linking} theme={navigationTheme}>
+      <NavigationContainer linking={setDeepLink()} theme={navigationTheme}>
         {children}
       </NavigationContainer>
     </ThemeContext.Provider>
