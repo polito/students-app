0710fe9c737f42663ff75683f7af70cc581e6dea
Emanuele Coricciati <73798800+emacoricciati@users.noreply.github.com>
2025-06-03 16:53:32 +0200

    fix(ui): fix controls color for webview in ios

--- src/core/hooks/useOpenInAppLink.ts
+++ src/core/hooks/useOpenInAppLink.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { useTheme } from '@lib/ui/hooks/useTheme.ts';
 
 import * as WebBrowser from 'expo-web-browser';
@@ -5,15 +7,18 @@ import * as WebBrowser from 'expo-web-browser';
 export const useOpenInAppLink = () => {
   const { colors } = useTheme();
 
-  return async (url: string) => {
-    await WebBrowser.openBrowserAsync(url, {
-      showTitle: true,
-      controlsColor: colors.headersBackground,
-      toolbarColor: colors.headersBackground,
-      secondaryToolbarColor: colors.headersBackground,
-      enableBarCollapsing: true,
-      showInRecents: true,
-      readerMode: false,
-    });
-  };
+  return useCallback(
+    async (url: string) => {
+      await WebBrowser.openBrowserAsync(url, {
+        showTitle: true,
+        controlsColor: colors.link,
+        toolbarColor: colors.headersBackground,
+        secondaryToolbarColor: colors.headersBackground,
+        enableBarCollapsing: true,
+        showInRecents: true,
+        readerMode: false,
+      });
+    },
+    [colors],
+  );
 };
