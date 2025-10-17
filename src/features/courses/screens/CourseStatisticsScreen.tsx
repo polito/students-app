53224b991632279c37d84adef829859b9e18587a
mattiamarine <mattia.marinelli03@gmail.com>
2025-10-17 13:44:08 +0200

    fix(statistics): delete unused vars and args

--- src/features/courses/screens/CourseStatisticsScreen.tsx
+++ src/features/courses/screens/CourseStatisticsScreen.tsx
@@ -33,15 +33,9 @@ import { EnrolledExamDetailChart } from '../components/EnrolledExamDetailChart.t
 import { computeStatisticsFilters } from '../utils/computeStatisticsFilters';
 
 type Props = NativeStackScreenProps<SharedScreensParamList, 'CourseStatistics'>;
-export const CourseStatisticsScreen = ({ route, navigation }: Props) => {
+export const CourseStatisticsScreen = ({ route }: Props) => {
   const { t } = useTranslation();
-  const {
-    courseShortcode: shortCode,
-    year,
-    teacherId,
-    filter,
-    nameCourse,
-  } = route.params;
+  const { courseShortcode: shortCode, year, teacherId, filter } = route.params;
 
   const { spacing, colors } = useTheme();
   const [currentFilters, setCurrentFilters] = useState<{
