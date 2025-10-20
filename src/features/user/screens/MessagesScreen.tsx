764c75caa35e0de04a69ed31eea999e812659da9
FabrizioCostaMedich <fabrizio.costamedich@gmail.com>
2025-10-20 02:17:32 +0200

    fix(profile): make the component generic

--- src/features/user/screens/MessagesScreen.tsx
+++ src/features/user/screens/MessagesScreen.tsx
@@ -1,18 +1,14 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
 
 import { useTranslation } from 'react-i18next';
 import { Pressable, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
-import Reanimated, {
-  SharedValue,
-  useAnimatedStyle,
-} from 'react-native-reanimated';
 
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { OverviewList } from '@lib/ui/components/OverviewList';
 import { RefreshControl } from '@lib/ui/components/RefreshControl';
 import { Section } from '@lib/ui/components/Section';
+import { Swipeable, SwipeableProvider } from '@lib/ui/components/Swipeable';
 import { useTheme } from '@lib/ui/hooks/useTheme';
 
 import { BottomBarSpacer } from '~/core/components/BottomBarSpacer.tsx';
@@ -23,33 +19,8 @@ import {
 
 import { MessageListItem } from '../components/MessageListItem';
 
-function createRightAction(onDelete: () => void) {
-  return (_: SharedValue<number>, drag: SharedValue<number>) => {
-    const { colors, palettes } = useTheme();
-    const styleAnimation = useAnimatedStyle(() => {
-      return {
-        transform: [{ translateX: drag.value + 88 }],
-      };
-    });
-
-    return (
-      <Reanimated.View style={styleAnimation}>
-        <Pressable
-          style={[
-            styles.deleteButton,
-            { backgroundColor: palettes.error[500] },
-          ]}
-          onPress={onDelete}
-        >
-          <FontAwesomeIcon icon={faTrash} color={colors.white} size={20} />
-        </Pressable>
-      </Reanimated.View>
-    );
-  };
-}
-
 export const MessagesScreen = () => {
-  const { spacing } = useTheme();
+  const { spacing, colors, palettes } = useTheme();
   const messagesQuery = useGetMessages();
   const { mutateAsync: deleteMessage } = useDeleteMessage();
 
@@ -67,33 +38,41 @@ export const MessagesScreen = () => {
     >
       <SafeAreaView>
         <GestureHandlerRootView>
-          <Section style={{ marginTop: spacing['2'] }}>
-            <OverviewList
-              loading={isLoading}
-              emptyStateText={t('messagesScreen.empty')}
-            >
-              {messages?.map((message, index) => (
-                <ReanimatedSwipeable
-                  key={message.id}
-                  friction={2}
-                  enableTrackpadTwoFingerGesture
-                  rightThreshold={40}
-                  overshootRight={false}
-                  renderRightActions={createRightAction(() =>
-                    handleDelete(message.id),
-                  )}
-                >
-                  <Reanimated.View>
+          <SwipeableProvider>
+            <Section style={{ marginTop: spacing['2'] }}>
+              <OverviewList
+                loading={isLoading}
+                emptyStateText={t('messagesScreen.empty')}
+              >
+                {messages?.map((message, index) => (
+                  <Swipeable
+                    key={message.id}
+                    rightAction={
+                      <Pressable
+                        style={[
+                          styles.deleteButton,
+                          { backgroundColor: palettes.error[500] },
+                        ]}
+                        onPress={() => handleDelete(message.id)}
+                      >
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          color={colors.white}
+                          size={20}
+                        />
+                      </Pressable>
+                    }
+                  >
                     <MessageListItem
                       messageItem={message}
                       index={index}
                       totalData={messages.length}
                     />
-                  </Reanimated.View>
-                </ReanimatedSwipeable>
-              ))}
-            </OverviewList>
-          </Section>
+                  </Swipeable>
+                ))}
+              </OverviewList>
+            </Section>
+          </SwipeableProvider>
         </GestureHandlerRootView>
         <BottomBarSpacer />
       </SafeAreaView>
