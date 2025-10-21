537c684f1582a198b70d4a5116c11950d8ac26c3
Federico Cucinella <10742159+QcFe@users.noreply.github.com>
2025-10-21 17:12:43 +0200

    refactor(infra): bump deps and strenghten ios initial setup (#739)
    * refactor(infra): bump deps and strenghten ios initial setup
    
    * fix(navigation): header close button

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['module:@react-native/babel-preset'],
     plugins: [
       '@babel/plugin-proposal-export-namespace-from',
       [
