65784cbd9438d090764c5bc9b795d2688b85ef62
giacomobelluardo <giacomino21@icloud.com>
2025-10-29 14:52:56 +0100

    feat(Navigation): added loading while computing the path from the API

--- src/features/places/components/PlacesListFooter.tsx
+++ src/features/places/components/PlacesListFooter.tsx
@@ -1,59 +1,64 @@
 import { memo } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { useStylesheet } from '@lib/ui/hooks/useStylesheet';
-import { CtaButton } from '@lib/ui/components/CtaButton';
 import { useTranslation } from 'react-i18next';
+import { StyleSheet, View } from 'react-native';
+
+import { CtaButton } from '@lib/ui/components/CtaButton';
+import { useStylesheet } from '@lib/ui/hooks/useStylesheet';
+
 interface PlacesListFooterProps {
-    computeButtonState: number;
-    startRoomLength: number;
-    destinationRoomLength: number;
-    handleComputeButtonState: (num: number) => void;
-    showItinerary: () => void;
+  computeButtonState: number;
+  startRoomLength: number;
+  destinationRoomLength: number;
+  isLoading: boolean;
+  handleComputeButtonState: (num: number) => void;
+  showItinerary: () => void;
 }
 
 const PlacesListFooterComponent = ({
-    computeButtonState,
-    startRoomLength,
-    destinationRoomLength,
-    handleComputeButtonState,
-    showItinerary,
+  computeButtonState,
+  startRoomLength,
+  destinationRoomLength,
+  handleComputeButtonState,
+  isLoading,
+  showItinerary,
 }: PlacesListFooterProps) => {
-    const styles = useStylesheet(createStyles);
-    const { t } = useTranslation();
-    const isDisabled = startRoomLength === 0 || destinationRoomLength === 0;
-    
-    return (
-        <View style={styles.ctaButtonContainer}>
-            <CtaButton
-                title={
-                    computeButtonState === 0 ? t('indicationsScreen.computePath') : t('indicationsScreen.showIndications')
-                }
-                action={() => {
-                    if(computeButtonState > 0 && !isDisabled){
-                        showItinerary();
-                    }
-                    else
-                        handleComputeButtonState(1);
-                }}
-                disabled={isDisabled}
-                style={{ width: '100%' }}
-            />
-        </View>
-    );
+  const styles = useStylesheet(createStyles);
+  const { t } = useTranslation();
+  const isDisabled = startRoomLength === 0 || destinationRoomLength === 0;
+
+  return (
+    <View style={styles.ctaButtonContainer}>
+      <CtaButton
+        title={
+          computeButtonState === 0
+            ? t('indicationsScreen.computePath')
+            : t('indicationsScreen.showIndications')
+        }
+        action={() => {
+          if (computeButtonState > 0 && !isDisabled) {
+            showItinerary();
+          } else handleComputeButtonState(1);
+        }}
+        disabled={isDisabled}
+        loading={isLoading}
+        style={{ width: '100%' }}
+      />
+    </View>
+  );
 };
 
 export const PlacesListFooter = memo(PlacesListFooterComponent);
 
-const createStyles = () => 
-    StyleSheet.create({
+const createStyles = () =>
+  StyleSheet.create({
     ctaButtonContainer: {
-        display: 'flex',
-        paddingHorizontal: 18,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 10,
-        alignSelf: 'stretch',
-        marginTop: '20%',
+      display: 'flex',
+      paddingHorizontal: 18,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 10,
+      alignSelf: 'stretch',
+      marginTop: '20%',
     },
-});
+  });
