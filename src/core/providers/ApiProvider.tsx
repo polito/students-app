059b28a021ff3ad0892694be88e2a9197ff83001
giacomobelluardo <giacomino21@icloud.com>
2025-11-10 15:47:05 +0100

    feat(navigation): added new things for error managing

--- src/core/providers/ApiProvider.tsx
+++ src/core/providers/ApiProvider.tsx
@@ -77,12 +77,10 @@ export const ApiProvider = ({ children }: PropsWithChildren) => {
 
         // The login alert is handled in the login screen
         if (!error.response.url.includes('/login')) {
-          if (!error.response.url.includes('/directions')) {
-            Alert.alert(
-              t('common.error'),
-              message ?? t('common.somethingWentWrong'),
-            );
-          }
+          Alert.alert(
+            t('common.error'),
+            message ?? t('common.somethingWentWrong'),
+          );
         }
 
         if (!isEnvProduction) {
